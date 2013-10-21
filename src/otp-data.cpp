7e7042069073744c1c2048415c3bfc715ecdcdfe
rfree <rfree@gmx.com>
2013-10-21 14:19:48 +0200

    First version, [wip]

---
+++ src/otp-data.cpp
@@ -0,0 +1,108 @@
+#include <stdio.h>      /* printf */
+#include <stdlib.h>     /* getenv */
+#include <iostream>
+#include <string>
+
+int main ()
+{
+	char a='a', z='z';
+	using namespace std;
+
+	string chars = string("^?!.*+-/=@$:"); 
+	/*
+
+? ! .
++ - * / 
+/ :
++ / =
+@
+#
+~
+
+
+^?!.*+-/=@$~
+
+
+
+	 
+	 */
+
+	int az = ((int)(z-a+1)) ;
+	int num = 10;
+	int ch = chars.size();
+	int all = az + num + ch;
+
+	string alpha="";
+	for (char i='A'; i<='Z'; ++i) { char cs[2]; cs[0]=i; cs[1]='\0'; alpha += string(cs); }
+	for (int i=0; i<=9; ++i) { char cs[2]; cs[0]='0'+i; cs[1]='\0'; alpha += string(cs); }
+	for (int i=0; i<chars.size(); ++i) { alpha += chars.at(i); }
+
+	alpha = "ABCDEFGHiJKLMNoPQRSTUVWXYZ0123456789^?!.*+-/=@$:"; // XXX
+//	alpha = "ABCD" ;
+
+
+	cout << alpha.size() << endl;
+	cout << alpha << endl;
+
+
+	if (1)
+	{
+		int nr=0;
+		cout << "\nRandom 6*8 table:" << endl;
+
+		int nrBig=1; // the 1..8 dice
+
+		cout << "   |";  	for (int x=1; x<=6; ++x) cout << x << (x<=6 ? "|" : " "); // header
+		cout << endl;
+		while (nr<alpha.size()) {
+
+			cout << nrBig<<"->|";
+
+			for (int x=1; x<=6; ++x) {
+				cout << alpha.at(nr) << (x<=6 ? "|" : " ");
+				++nr;
+			}
+			cout << endl;
+
+			if ((0==(nrBig%4)) && (nrBig<7)) { cout<<"   |"; 
+				for(int x=1; x<=6; ++x) { cout<<"-|"; } 
+				cout << endl; 
+			} 
+
+			++nrBig;
+		}
+	}
+	
+
+
+	{
+		int nr=0;
+		cout << "\n the ROT table:" << endl;
+
+		int S = alpha.size();
+
+
+		for (int y=0; y<S; ++y) {
+			if ((0==(y%5)) && (y<700)) { 
+cout<<endl;
+				cout << ">>>>>║";  	for (int x=0; x<S; ++x) cout << alpha.at(x) << "║" ; // header
+				cout << "<<<<<" << endl;
+			} 
+
+			char c=alpha.at(y);
+			cout << c<<c<<c <<"->┊";
+
+			for (int x=0; x<S; ++x) {
+				int nr = (x+y)%S;
+				cout << alpha.at(nr) << "┊";
+				// ║┊
+			}
+			cout << endl;
+
+		}
+	}
+	
+
+
+}
+
